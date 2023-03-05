40ae4e6d776336579aabc315696f618588beddf8
Etienne Collin <collin.etienne.contact@gmail.com>
2023-03-05 14:46:39 -0500

    Fixed quicksort time complexity in comment

--- com/etiennecollin/tp1/Guild.java
+++ com/etiennecollin/tp1/Guild.java
@@ -18,14 +18,16 @@ public class Guild {
     private final Bank bank = new Bank();
     /**
      * We implemented a main heroCategories array which contains LinkedLists (used as queues) for heroes of each category.
-     * As the index of each queue in the main array match the category of hero they contain,
-     * finding heroes with the right category is extremely efficient. Indeed, we only need to check if the queue is empty or not.
-     * The time complexity is O(1) for the hero-category-related operations as the lookup in the main array is O(1) and
-     * the add(), getFirst() and removeFirst() operations in the linked lists are also O(1).
-     * If we had implemented a single main list, we would have had to do multiple passes to find an hero of a certain level.
-     * In the worst case, we would have had to check all the elements of the array list 5 times (because there are 5 hero levels).
-     * It would have also been possible to sort the elements of the arrayList before searching for an element, but this
-     * would have been inefficient because sorting complexity is O(n).
+     * As the index of each queue in the main array match the category of hero they contain, finding heroes with the
+     * right category is extremely efficient. Indeed, we only need to check if the queue is empty or not. The time
+     * complexity is O(1) for the hero-category-related operations as the lookup in the main array is O(1) and the add(),
+     * getFirst() and removeFirst() operations in the linked lists are also O(1).
+     *
+     * If we had implemented a single main list, we would have had to do multiple passes to find an hero of a certain
+     * level. In the worst case, we would have had to check all the elements of the array list 5 times (because there
+     * are 5 hero levels). It would have also been possible to sort the elements of the arrayList before searching for
+     * an element, but this would have been inefficient because sorting complexity is O(nlog(n)) (in the case of
+     * quicksort).
      */
     private final LinkedList<Hero> heroesCategory1 = new LinkedList<>();
     private final LinkedList<Hero> heroesCategory2 = new LinkedList<>();

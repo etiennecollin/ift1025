78882c9d58db48fcbc9b656d8502f476b083e710
Etienne Collin <collin.etienne.contact@gmail.com>
2023-03-14 22:19:26 -0400

    Cleanup

--- javadoc/tp1/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.etiennecollin.tp1"},{"l":"com.etiennecollin.tp1.guildcommands"},{"l":"com.etiennecollin.tp1.hero"}];updateSearchResults();
--- javadoc/tp2/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.etiennecollin.tp2","l":"Main"}];updateSearchResults();

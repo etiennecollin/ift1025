d9cc653399f00798bf337b350871819b56796542
Etienne Collin <collin.etienne.contact@gmail.com>
2023-03-14 22:21:29 -0400

    Initialize project

---
+++ src/main/java/com/etiennecollin/tp2/Main.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023. Etienne Collin #20237904 & Justin Villeneuve #20132792
+ */
+
+package com.etiennecollin.tp2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

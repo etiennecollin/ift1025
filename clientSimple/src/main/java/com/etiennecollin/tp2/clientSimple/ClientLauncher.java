c1cab550f53a94cfc2d3ee655ec6c50cb504514c
Etienne Collin <collin.etienne.contact@gmail.com>
2023-03-23 14:27:47 -0400

    Handled the "no server available" exception

--- clientSimple/src/main/java/com/etiennecollin/tp2/clientSimple/ClientLauncher.java
+++ clientSimple/src/main/java/com/etiennecollin/tp2/clientSimple/ClientLauncher.java
@@ -5,6 +5,7 @@
 package com.etiennecollin.tp2.clientSimple;
 
 import java.io.IOException;
+import java.net.ConnectException;
 
 /**
  * The ClientLauncher class launches a client to bind to a specific server port and starts its execution.
@@ -26,8 +27,11 @@ public class ClientLauncher {
     public static void main(String[] args) {
         try {
             System.out.println("[Client] Running...");
-        } catch (IOException | ClassNotFoundException e) {
             Client.run(PORT);
+        } catch (ConnectException e) {
+            System.err.println(e.getMessage() + ", no server available on port " + PORT + ".");
+        }
+        catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
